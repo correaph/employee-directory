8a5c7cdca7b39569c611d07a42a0f9a905b65ff9
Paulo Correa <henriqueph@hotmail.com>
2020-10-19 19:35:30 -0300

    Updates

---
+++ precache-manifest.a2db94435a5b863cccc092278f7c181b.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "3d657ba1a65ac68094b514bdd05ea5db",
+    "url": "/employee-directory/index.html"
+  },
+  {
+    "revision": "38212dc26e2f20981f2c",
+    "url": "/employee-directory/static/css/main.5ecd60fb.chunk.css"
+  },
+  {
+    "revision": "096b49c719130b8101bb",
+    "url": "/employee-directory/static/js/2.874cf352.chunk.js"
+  },
+  {
+    "revision": "570d362d673dab785e62d2b8563e1118",
+    "url": "/employee-directory/static/js/2.874cf352.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "38212dc26e2f20981f2c",
+    "url": "/employee-directory/static/js/main.2456af7b.chunk.js"
+  },
+  {
+    "revision": "79d7c4f4ad2a4d373d4e",
+    "url": "/employee-directory/static/js/runtime-main.759497ab.js"
+  }
+]);
